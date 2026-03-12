e28e802350c04fce5a0cd2466a50968b3ddb3231
John Smith <ksgkdu96@gmail.com>
2026-03-13 05:28:30 +0800

    Add request logging middleware

---
+++ app/middleware.py
@@ -0,0 +1,9 @@
+from fastapi import Request
+import time
+
+async def log_requests(request: Request, call_next):
+    start = time.time()
+    response = await call_next(request)
+    duration = time.time() - start
+    response.headers["X-Process-Time"] = str(duration)
+    return response
