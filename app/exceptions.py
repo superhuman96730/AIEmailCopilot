77eda3281d5a2b882ef1a484bb4aa6a27ecffdf5
John Smith <ksgkdu96@gmail.com>
2026-03-13 05:28:29 +0800

    Add custom exception classes

---
+++ app/exceptions.py
@@ -0,0 +1,11 @@
+class EmailNotFoundError(Exception):
+    pass
+
+class ClassificationFailedError(Exception):
+    pass
+
+class DatabaseError(Exception):
+    pass
+
+class AuthenticationError(Exception):
+    pass
