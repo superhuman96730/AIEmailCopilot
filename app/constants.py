bd213429751dff65b5dad019205297bcf2845417
John Smith <ksgkdu96@gmail.com>
2026-03-13 05:29:54 +0800

    Add application constants

---
+++ app/constants.py
@@ -0,0 +1,8 @@
+# Email Classification Constants
+CATEGORIES = ["work", "personal", "promotional", "spam"]
+PRIORITIES = ["high", "normal", "low"]
+
+# Processing Constants
+MAX_EMAIL_SIZE = 10_000_000  # 10MB
+BATCH_SIZE = 100
+CACHE_TTL = 3600  # 1 hour
